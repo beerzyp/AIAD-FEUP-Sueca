7183771c7d73500b8b4559d424eee265bbcadc99
unknown <gomespedromiguel@hotmail.com>
2018-10-27 16:18:51 +0100

    mostrar as cartas no final de cada ronda

--- src/CheckWinnerBehaviour.java
+++ src/CheckWinnerBehaviour.java
@@ -11,6 +11,27 @@ public class CheckWinnerBehaviour extends OneShotBehaviour {
 	public void action() {
 		int lastRoundWinner=this.sueca.getGameLogic().winner(ronda, sueca);
 		System.out.println("VENCEDOR RONDA: player"+ lastRoundWinner+"\n");
+		
+		System.out.println("\nJogador 1:" + sueca.getPlayer1().getPlayerHand().getMao().size());
+		for(int i=0;i<sueca.getPlayer1().getPlayerHand().getMao().size();i++) {
+			System.out.println(sueca.getPlayer1().getPlayerHand().getMao().get(i).toString());
+			
+		}
+		System.out.println("\nJogador 2:" + sueca.getPlayer2().getPlayerHand().getMao().size());
+		for(int i=0;i<sueca.getPlayer2().getPlayerHand().getMao().size();i++) {
+			System.out.println(sueca.getPlayer2().getPlayerHand().getMao().get(i).toString());
+			
+		}
+		System.out.println("\nJogador 3:" + sueca.getPlayer3().getPlayerHand().getMao().size());
+		for(int i=0;i<sueca.getPlayer3().getPlayerHand().getMao().size();i++) {
+			System.out.println(sueca.getPlayer3().getPlayerHand().getMao().get(i).toString());
+			
+		}
+		System.out.println("\nJogador 4:" + sueca.getPlayer4().getPlayerHand().getMao().size());
+		for(int i=0;i<sueca.getPlayer4().getPlayerHand().getMao().size();i++) {
+			System.out.println(sueca.getPlayer4().getPlayerHand().getMao().get(i).toString());
+			
+		}
 	}
 
 }
