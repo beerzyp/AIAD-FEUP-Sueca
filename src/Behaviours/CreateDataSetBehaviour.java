49ae1a961f5efd652ec714c35d04339aa2a5b7f1
beerzyp <up201505092@fe.up.pt>
2018-12-11 23:18:27 +0000

    datasetChangesValueOf

--- src/Behaviours/CreateDataSetBehaviour.java
+++ src/Behaviours/CreateDataSetBehaviour.java
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import Agents.GameAGENT;
 import GameLogic.Carta;
@@ -30,11 +31,11 @@ public class CreateDataSetBehaviour extends OneShotBehaviour {
 		percepton=flag;
 		calculateInitialScore ();
 		player1Hand= new ArrayList<Carta>(this.sueca.getPlayer1InitialDeck());
-    	player2Hand= new ArrayList<Carta>(this.sueca.getPlayer2InitialDeck());
-    	player3Hand= new ArrayList<Carta>(this.sueca.getPlayer3InitialDeck());
-    	player4Hand= new ArrayList<Carta>(this.sueca.getPlayer4InitialDeck());
+		player2Hand= new ArrayList<Carta>(this.sueca.getPlayer2InitialDeck());
+		player3Hand= new ArrayList<Carta>(this.sueca.getPlayer3InitialDeck());
+		player4Hand= new ArrayList<Carta>(this.sueca.getPlayer4InitialDeck());
 	}
-	
+
 	public  void calculateInitialScore (){
 		teamAPoints=0;
 		teamBPoints=0;
@@ -66,7 +67,7 @@ public class CreateDataSetBehaviour extends OneShotBehaviour {
 			return player4Hand;
 		default:
 			return null;
-		
+
 		}
 	}
 	@Override
@@ -82,58 +83,67 @@ public class CreateDataSetBehaviour extends OneShotBehaviour {
 		}
 		BufferedWriter bw= new BufferedWriter(fw);
 		pw = new PrintWriter(bw);
-        StringBuilder sb = new StringBuilder();
-        if(this.percepton) {
-            sb.append("teamAiPoints"); //jogNum
-	        sb.append(',');
-	        sb.append("teamAfPoints"); //jog.cardValue
-	        sb.append(',');
-            sb.append("teamBiPoints"); //jogNum
-	        sb.append(',');
-	        sb.append("teamBfPoints"); //jog.cardValue
-	        sb.append('\n');
-	        
-	        sb.append(this.teamAPoints); //jogNum
-	        sb.append(',');
-	        sb.append(Integer.toString(GameAGENT.getTeamPointsA())); //final Score PLay 1 and 3
-	        sb.append(',');
-            sb.append(this.teamBPoints); //jogNum
-	        sb.append(',');
-	        sb.append(Integer.toString(GameAGENT.getTeamPointsB())); //ply 2 and 4 final score
-	        sb.append('\n');
-        	
-        }
-        else {
-	        for(int i=0;i<4;i++) {
-		        sb.append("playerNum" + (i+1)); //jogNum
-		        sb.append(',');
-		        sb.append("card" + (i+1)); //jog.cardValue
-		        sb.append(',');
-		        sb.append("suit" + (i+1));//jog.Suit
-		        sb.append(',');
-	        }
-	        sb.append("roundWinningPlayer");//jog.Suit
-	        sb.append(',');
-	        sb.append("handNumber");//jog.Suit
-	        sb.append(',');
-	        sb.append("trunfo");//jog.Suit
-	        sb.append(',');
-	        sb.append("numOfSpadesinHand");
-	        sb.append(',');
-	        sb.append("numOfCopasInHand");
-	        sb.append(',');
-	        sb.append("numOfPausInHand");
-	        sb.append(',');
-	        sb.append("numOfOurosInHand");
-	        sb.append("\n");//jog.Suit
-	        int playerId3rdPosition=3;
+		StringBuilder sb = new StringBuilder();
+		if(this.percepton) {
+			sb.append("teamAiPoints"); //jogNum
+			sb.append(',');
+			sb.append("teamAfPoints"); //jog.cardValue
+			sb.append(',');
+			sb.append("teamBiPoints"); //jogNum
+			sb.append(',');
+			sb.append("teamBfPoints"); //jog.cardValue
+			sb.append('\n');
+
+			sb.append(this.teamAPoints); //jogNum
+			sb.append(',');
+			sb.append(Integer.toString(GameAGENT.getTeamPointsA())); //final Score PLay 1 and 3
+			sb.append(',');
+			sb.append(this.teamBPoints); //jogNum
+			sb.append(',');
+			sb.append(Integer.toString(GameAGENT.getTeamPointsB())); //ply 2 and 4 final score
+			sb.append('\n');
+
+		}
+		else {
+			for(int i=0;i<4;i++) {
+				sb.append("playerNum" + (i+1)); //jogNum
+				sb.append(',');
+				sb.append("card" + (i+1)); //jog.cardValue
+				sb.append(',');
+				sb.append("suit" + (i+1));//jog.Suit
+				sb.append(',');
+			}
+			sb.append("roundWinningPlayer");//jog.Suit
+			sb.append(',');
+			sb.append("handNumber");//jog.Suit
+			sb.append(',');
+			sb.append("trunfo");//jog.Suit
+			sb.append(',');
+			sb.append("numOfSpadesinHand");
+			sb.append(',');
+			sb.append("numOfCopasInHand");
+			sb.append(',');
+			sb.append("numOfPausInHand");
+			sb.append(',');
+			sb.append("numOfOurosInHand");
+			sb.append(',');
+			sb.append("valueOfSpades");
+			sb.append(',');
+			sb.append("valueOfCopas");
+			sb.append(',');
+			sb.append("valueOfPaus");
+			sb.append(',');
+			sb.append("valueOfOuros");
+			sb.append("\n");//jog.Suit
+			int playerId3rdPosition=3;
 			for(int i=0;i<this.rondas.size();i++) {
 				for(int j=0;j<this.rondas.get(i).returnTableHand().size();j++) {//maos da ronda
-					
+
 					Carta c1 =this.rondas.get(i).returnTableHand().get(j).getKey();
 					int player = this.rondas.get(i).returnTableHand().get(j).getValue();
 					if(j==2) {//PLAYER 3 DATA SET EVALs
 						playerId3rdPosition=player;
+						
 					}
 					if(player==1) {
 						player1Hand.remove(c1);
@@ -147,69 +157,127 @@ public class CreateDataSetBehaviour extends OneShotBehaviour {
 					else if (player==4) {
 						player4Hand.remove(c1);
 					}
-					
-			        sb.append(player); //jogNum
-			        sb.append(',');
-			        sb.append(c1.getDataSetCardValue()); //jog.cardValue
-			        sb.append(',');
-			        sb.append(c1.getNaipe());//jog.Suit
-			        sb.append(',');
+
+					sb.append(player); //jogNum
+					sb.append(',');
+					sb.append(c1.getDataSetCardValue()); //jog.cardValue
+					sb.append(',');
+					sb.append(c1.getNaipe());//jog.Suit
+					sb.append(',');
 				}
 				int lastRoundWinner=this.sueca.getGameLogic().winner(this.rondas.get(i), sueca);
-		        sb.append(lastRoundWinner);//WinningHandPlayer
-		        sb.append(',');
-		        sb.append(i+1);//Hand number
-		        sb.append(',');
+				sb.append(lastRoundWinner);//WinningHandPlayer
+				sb.append(',');
+				sb.append(i+1);//Hand number
+				sb.append(',');
 				sb.append(this.sueca.getTrunfo().getNaipe()); //trunfo jogo
-			    sb.append(',');
-		      	StringBuilder sp=writeVectorToCsv(this.sueca.getTrunfo().convertToDataSetArray(this.getPlayerN(playerId3rdPosition)));	    
-		      	sb.append(sp);
-		      	sb.append('\n');
+				sb.append(',');
+				StringBuilder sp=writeVectorToCsv(this.sueca.getTrunfo().convertToDataSetArray(this.getPlayerN(playerId3rdPosition)));	    
+				sb.append(sp);
+				StringBuilder sr= writeNumOfNaipeToCsv(valueOfNaipe(this.getPlayerN(playerId3rdPosition)));	    
+				sb.append(sr);
+				sb.append('\n');
 			}
 		}
-        pw.write(sb.toString());
-        pw.close();
-	
+		pw.write(sb.toString());
+		pw.close();
+
 	}
 
+
+	// Arrays.sort(arr); 
+	private ArrayList<Pair<Integer,Integer>> valueOfNaipe(ArrayList<Carta> convertToDataSetArray) {
+		ArrayList<Integer> spades = new ArrayList<Integer>(),copas = new ArrayList<Integer>(),paus = new ArrayList<Integer>(),ouros = new ArrayList<Integer>();
+		for(int i=0;i<convertToDataSetArray.size();i++) {
+			if(convertToDataSetArray.get(i).getNaipe()==0) {
+				spades.add(convertToDataSetArray.get(i).getDataSetCardValue());
+			}
+			else if(convertToDataSetArray.get(i).getNaipe()==1) {
+				copas.add(convertToDataSetArray.get(i).getDataSetCardValue());
+
+			}
+			else if(convertToDataSetArray.get(i).getNaipe()==2) {
+				paus.add(convertToDataSetArray.get(i).getDataSetCardValue());
+			}
+			else if(convertToDataSetArray.get(i).getNaipe()==3) {
+				ouros.add(convertToDataSetArray.get(i).getDataSetCardValue());
+			}
+			else return null;
+
+		}
+		ArrayList<Pair<Integer,Integer>> array = new ArrayList<Pair<Integer,Integer>> ();
+		int maxSpade,maxCopas,maxPaus,maxOuros;
+		if(!spades.isEmpty())
+			maxSpade=Collections.max(spades);
+		else maxSpade=0;
+		if(!copas.isEmpty())
+			maxCopas=Collections.max(copas);
+		else maxCopas=0;
+		if(!paus.isEmpty())
+			maxPaus=Collections.max(paus);
+		else maxPaus=0;
+		if(!ouros.isEmpty())
+			maxOuros=Collections.max(ouros);
+		else maxOuros=0;
+		array.add(new Pair<Integer,Integer>(0,maxSpade));
 	
+		array.add(new Pair<Integer,Integer>(1,maxCopas));
+		array.add(new Pair<Integer,Integer>(2,maxPaus));
+		array.add(new Pair<Integer,Integer>(3,maxOuros));
+		return array;
+	}
+	private StringBuilder writeNumOfNaipeToCsv(ArrayList<Pair<Integer,Integer>> array) {
+		StringBuilder sp=new StringBuilder();
+		sp.append(array.get(0).getValue());
+		sp.append(',');
+		sp.append(array.get(1).getValue());
+		sp.append(',');
+		sp.append(array.get(2).getValue());
+		sp.append(',');
+		sp.append(array.get(3).getValue());
+		sp.append(',');
+			
+		
+		return sp;
+		
+	}
 
 	private StringBuilder writeVectorToCsv(ArrayList<Pair<Integer, Integer>> convertToDataSetArray) {
-		  StringBuilder sp = new StringBuilder();
-		  /*
-		   * 		case ESPADAS: return 0; 
+		StringBuilder sp = new StringBuilder();
+		/*
+		 * 		case ESPADAS: return 0; 
 		case COPAS: return 1; 
 		case PAUS: return 2; 
 		case OUROS: return 3; 
-		   */
-		  int numOfSpades = 0,Copas = 0,Paus = 0,Ouros = 0;
-		  for(int i=0;i<convertToDataSetArray.size();i++) {
-			 if(convertToDataSetArray.get(i).getValue()==0) {
-				 numOfSpades++;
-			 }
-			 if(convertToDataSetArray.get(i).getValue()==1) {
-				 Copas++;
-			 }
-			 if(convertToDataSetArray.get(i).getValue()==2) {
-				 Paus++;
-			 }
-			 if(convertToDataSetArray.get(i).getValue()==3) {
-				 Ouros++;
-			 }
-			 
-			 }
-		  sp.append(numOfSpades);
-		  sp.append(',');
-		  sp.append(Copas);
-		  sp.append(',');
-		  sp.append(Paus);
-		  sp.append(',');
-		  sp.append(Ouros);
-		  sp.append(',');
-		  return sp;
-		
+		 */
+		int numOfSpades = 0,Copas = 0,Paus = 0,Ouros = 0;
+		for(int i=0;i<convertToDataSetArray.size();i++) {
+			if(convertToDataSetArray.get(i).getValue()==0) {
+				numOfSpades++;
+			}
+			if(convertToDataSetArray.get(i).getValue()==1) {
+				Copas++;
+			}
+			if(convertToDataSetArray.get(i).getValue()==2) {
+				Paus++;
+			}
+			if(convertToDataSetArray.get(i).getValue()==3) {
+				Ouros++;
+			}
+
+		}
+		sp.append(numOfSpades);
+		sp.append(',');
+		sp.append(Copas);
+		sp.append(',');
+		sp.append(Paus);
+		sp.append(',');
+		sp.append(Ouros);
+		sp.append(',');
+		return sp;
+
 	}
-	
+
 	/*
 	 * 1) S1 "Suit of card #1" 
 Ordinal (1-4) representing {Hearts, Spades, Diamonds, Clubs} 
@@ -235,8 +303,8 @@ Ordinal (1-4) representing {Hearts, Spades, Diamonds, Clubs}
 8) C4 "Rank of card #4" 
 Numerical (1-13) representing (Ace, 2, 3, ... , Queen, King) 
 	 */
-	
-	
+
+
 
 
 }
